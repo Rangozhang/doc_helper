456b6cf12f926a48b4545139d3538b01b222a9b8
Rango <rangozhang@sina.com>
2015-12-14 16:09:34 -0500

    little change

--- Crawler.py
+++ Crawler.py
@@ -7,17 +7,20 @@ def replace(m_str, frm, to):
     first, second, third = m_str.rpartition(frm)
     return ''.join([first, to])
 
-def getSave(http):
+def getSave(arg):
+    http = arg.http
+    target = arg.target
     fd = url.urlopen(http)
     items = re.findall('<a href="(.*pdf)">', fd.read())
     for item in items:
         print item
-        out_file = open(item, 'wb')
+        out_file = open(os.path.join(target, item), 'wb')
         out_file.write(url.urlopen(replace(http.strip(),'handouts.html',item)).read())
         out_file.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('http')
+    parser.add_argument('-s','--http')
+    parser.add_argument('-t','--target')
     arg = parser.parse_args()
-    getSave(arg.http)
+    getSave(arg)
